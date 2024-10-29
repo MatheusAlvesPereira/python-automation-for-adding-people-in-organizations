6b5ce2ce40af1e292cb8eb70d0f11f5bd810d250
Matheus Alves <matheusalvessilvabr@gmail.com>
2024-10-29 16:12:22 -0300

    rmv error of main import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from main import Flask, request, jsonify
+from flask import Flask, request, jsonify
 import requests
 import os
 
@@ -7,16 +7,16 @@ app = Flask(__name__)
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 ORGANIZATION = os.getenv('ORGANIZATION')
 
-@app.route("/webhook", method=['POST'])
+@app.route("/webhook", methods=['POST'])
 def github_webhook():
     data = request.json
-    if request.headers.get('X-GitHub-Event') == 'star':
+    if request.headers.get('X-GitHub-Event') == 'follow':
         sender = data['sender']
         user = sender['login']
         send_invite(user)
         return jsonify({"message": f"Convite enviado para {user}"}), 200
 
-    return jsonify({"message": "Evento não é uma estrela"}), 400
+    return jsonify({"message": "Evento não é um follow"}), 400
 
 def send_invite(user):
     url = f"https://api.github.com/orgs/{ORGANIZATION}/invitations"
